fd7653d763f120e853428c5fee986bd302d43263
Muin-Islam <111909403+Muin-Islam@users.noreply.github.com>
2025-05-22 23:25:08 +0600

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,11 @@ import os
 # ----------------- App / extensions -----------------
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode="eventlet", manage_session=False)
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use PORT from Render or default to 5000
-    socketio.run(app, host='0.0.0.0', port=port)
-
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, host="0.0.0.0", port=port)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
